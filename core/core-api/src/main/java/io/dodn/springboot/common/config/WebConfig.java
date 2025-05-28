7a4a8952584d7c2753b63149aca73df1aa56fd7b
taemin6536 <xoals6536@naver.com>
2025-05-28 17:31:15 +0900

    feat: cors 설정 변경

--- core/core-api/src/main/java/io/dodn/springboot/common/config/WebConfig.java
+++ core/core-api/src/main/java/io/dodn/springboot/common/config/WebConfig.java
@@ -1,6 +1,7 @@
 package io.dodn.springboot.common.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,8 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 설정 적용
-                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://43.202.199.39") // 허용할 출처 지정 여러개 가능
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH") // 허용할 HTTP 메서드
+                .allowedOriginPatterns("http://localhost:*","https://localhost:*","http://43.202.199.39")
+//                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://43.202.199.39") // 허용할 출처 지정 여러개 가능
+                .allowedMethods(HttpMethod.GET.name(), HttpMethod.PATCH.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(), HttpMethod.DELETE.name()) // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 요청 헤더 허용
                 .allowCredentials(true) // 자격 증명(쿠키 등) 허용
                 .maxAge(3600); // Preflight 요청 캐시 시간 (1시간)
--- core/core-api/src/main/java/io/dodn/springboot/core/api/controller/HealthDocs.java
+++ core/core-api/src/main/java/io/dodn/springboot/core/api/controller/HealthDocs.java
@@ -1,20 +1,19 @@
 package io.dodn.springboot.core.api.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 
 public interface HealthDocs {
-    @Operation(summary = "Health Check API", description = "서버 통신 여부 기능", tags = {"Health"})
+    @Operation(
+            summary = "애플리케이션 상태 확인",
+            description = "애플리케이션의 동작 상태를 확인하는 헬스체크 엔드포인트입니다.",
+            tags = {"Health Check"}
+    )
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "성공"
-//                    ,content = @Content(mediaType = "application/json"
-//                    ,schema = @Schema(implementation = Void.class))
-            ),
-            @ApiResponse(responseCode = "404", description = "통신 불가능")
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "503", description = "통신 이상")
     })
     public ResponseEntity<Void> health();
 }
