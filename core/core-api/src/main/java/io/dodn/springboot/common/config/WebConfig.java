40c4b646673fe6f0fbff49b29ccf7bc74f1c7129
taemin6536 <xoals6536@naver.com>
2025-05-29 17:28:43 +0900

    fix: 빌드안되던 버그수정

--- core/core-api/src/main/java/io/dodn/springboot/common/config/WebConfig.java
+++ core/core-api/src/main/java/io/dodn/springboot/common/config/WebConfig.java
@@ -10,8 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 설정 적용
-                .allowedOriginPatterns("http://localhost:*","https://localhost:*","http://43.202.199.39")
-//                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://43.202.199.39") // 허용할 출처 지정 여러개 가능
+                .allowedOriginPatterns("http://localhost:*", "https://localhost:*", "http://43.202.199.39")
                 .allowedMethods(HttpMethod.GET.name(), HttpMethod.PATCH.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(), HttpMethod.DELETE.name()) // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 요청 헤더 허용
                 .allowCredentials(true) // 자격 증명(쿠키 등) 허용
