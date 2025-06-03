22e9f7c5fa9badc8f40feb962f8735617e432217
taemin6536 <xoals6536@naver.com>
2025-06-03 22:50:41 +0900

    feat: 카카오 로그인 oauth 1차 커밋

--- core/core-api/src/main/java/io/dodn/springboot/auth/jwt/JwtAuthenticationFilter.java
+++ core/core-api/src/main/java/io/dodn/springboot/auth/jwt/JwtAuthenticationFilter.java
@@ -4,11 +4,17 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final static Logger log = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
     private final JwtTokenProvider jwtTokenProvider;
     private static final String AUTHORIZATION_HEADER = "Authorization";
     private static final String BEARER_PREFIX = "Bearer ";
@@ -23,6 +29,27 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             final HttpServletResponse response,
             final FilterChain filterChain
     ) throws ServletException, IOException {
+        String token = resolveToken(request);
 
+        if (!StringUtils.hasText(token)) {
+            log.info("토큰이 존재하지 않습니다.");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        if (StringUtils.hasText(token) || jwtTokenProvider.validateToken(token)) {
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String resolveToken(final HttpServletRequest request) {
+        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
+            return bearerToken.substring(BEARER_PREFIX.length());
+        }
+        return null;
     }
 }
