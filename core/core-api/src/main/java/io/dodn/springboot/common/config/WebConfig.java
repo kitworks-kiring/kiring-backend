e0ebded45c2c71d62ae517f935b1195bc9c78e0c
taemin6536 <xoals6536@naver.com>
2025-06-04 22:01:45 +0900

    feat: @LoginUser 어노테이션으로 Token 속의 memberId 꺼낼수있도록 로직 수정

--- core/core-api/src/main/java/io/dodn/springboot/common/config/WebConfig.java
+++ core/core-api/src/main/java/io/dodn/springboot/common/config/WebConfig.java
@@ -1,20 +1,22 @@
 package io.dodn.springboot.common.config;
 
+import io.dodn.springboot.common.annotation.LoginUserArgumentResolver;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
 
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**") // 모든 경로에 대해 CORS 설정 적용
-//            .allowedOriginPatterns("http://localhost:*", "https://localhost:*", "http://43.202.199.39")
-//            .allowedMethods(HttpMethod.GET.name(), HttpMethod.PATCH.name(), HttpMethod.POST.name(),
-//                    HttpMethod.PUT.name(), HttpMethod.DELETE.name()) // 허용할 HTTP 메서드
-//            .allowedHeaders("*") // 모든 요청 헤더 허용
-//            .allowCredentials(true) // 자격 증명(쿠키 등) 허용
-//            .maxAge(3600); // Preflight 요청 캐시 시간 (1시간)
-//    }
+    public WebConfig(final LoginUserArgumentResolver loginUserArgumentResolver) {
+        this.loginUserArgumentResolver = loginUserArgumentResolver;
+    }
 
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginUserArgumentResolver); // 커스텀 리졸버 등록
+    }
 }
