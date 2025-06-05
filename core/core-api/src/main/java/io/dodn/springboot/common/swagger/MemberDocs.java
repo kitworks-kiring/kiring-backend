6e0e0dbd40367b52430f6594682e9a7c012b21bf
taemin6536 <xoals6536@naver.com>
2025-06-05 17:58:48 +0900

    feat: 스웨거 수정

--- core/core-api/src/main/java/io/dodn/springboot/common/swagger/MemberDocs.java
+++ core/core-api/src/main/java/io/dodn/springboot/common/swagger/MemberDocs.java
@@ -13,20 +13,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 public interface MemberDocs {
 
-    @Operation(summary = "회원 정보 조회", description = "특정 회원의 정보를 조회하는 엔드포인트입니다.",
-            tags = { "Member Management" })
+    @Operation(summary = "회원 정보 조회", description = "특정 회원의 정보를 조회하는 엔드포인트입니다.", tags = { "Member Management" })
     @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "성공"),
             @ApiResponse(responseCode = "404", description = "회원이 존재하지 않음") })
     io.dodn.springboot.common.support.response.ApiResponse<MemberResponse> getMemberById(@LoginUser Long memberId);
 
-    @Operation(summary = "팀과 함께 회원 목록 조회", description = "현재 등록된 회원의 목록을 팀 정보와 함께 조회하는 엔드포인트입니다.",
-            tags = { "Member Management" })
+    @Operation(summary = "팀과 함께 회원 목록 조회", description = "현재 등록된 회원의 목록을 팀 정보와 함께 조회하는 엔드포인트입니다.", tags = { "Member Management" })
     @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "성공"),
             @ApiResponse(responseCode = "404", description = "회원이 존재하지 않음") })
     io.dodn.springboot.common.support.response.ApiResponse<MembersResponse> getMembersWithTeam();
 
-    @Operation(summary = "팀별 회원 목록 조회", description = "특정 팀에 속한 회원들의 목록을 조회하는 엔드포인트입니다.",
-            tags = { "Member Management" })
+    @Operation(summary = "팀별 회원 목록 조회", description = "특정 팀에 속한 회원들의 목록을 조회하는 엔드포인트입니다.", tags = { "Member Management" })
     @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "성공"),
             @ApiResponse(responseCode = "404", description = "팀이 존재하지 않음") })
     io.dodn.springboot.common.support.response.ApiResponse<MembersResponse> getMemberByTeam(
@@ -44,4 +41,8 @@ public interface MemberDocs {
             @PathVariable("memberId") final Long memberId,
             @RequestBody UpdateMemberRequest updateMemberRequest
     );
+
+
+    @Operation(summary = "권한 테스트용", description = "권한 테스트 엔드포인트입니다.", tags = { "Member Management" })
+    io.dodn.springboot.common.support.response.ApiResponse<String> getTest();
 }
