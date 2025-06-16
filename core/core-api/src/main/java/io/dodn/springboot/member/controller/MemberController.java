ef014d2322b9184a46c815cfa51dd9f0b09543ac
taemin6536 <xoals6536@naver.com>
2025-06-16 09:33:59 +0900

    feat: Member Images 조회 API 추가

--- core/core-api/src/main/java/io/dodn/springboot/member/controller/MemberController.java
+++ core/core-api/src/main/java/io/dodn/springboot/member/controller/MemberController.java
@@ -4,9 +4,11 @@ import io.dodn.springboot.common.annotation.LoginUser;
 import io.dodn.springboot.common.support.response.ApiResponse;
 import io.dodn.springboot.common.swagger.MemberDocs;
 import io.dodn.springboot.member.controller.request.UpdateMemberRequest;
+import io.dodn.springboot.member.controller.response.ImageUrlResponse;
 import io.dodn.springboot.member.controller.response.MemberResponse;
 import io.dodn.springboot.member.controller.response.MembersResponse;
 import io.dodn.springboot.member.domain.MemberService;
+import io.dodn.springboot.storage.db.member.entity.Member;
 import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/member")
@@ -57,6 +61,16 @@ public class MemberController implements MemberDocs {
             @RequestBody UpdateMemberRequest updateMemberRequest
     ) {
         return ApiResponse.success(MemberResponse.of(memberService.updateMember(memberId, updateMemberRequest.toEntity())));
+    }
+
+    @GetMapping("/members/images")
+    public ApiResponse<List<ImageUrlResponse>> getMemberUrls(){
+        List<Member> allMembers = memberService.findAllMembersWithTeamUsingFetchJoin();
+        List<ImageUrlResponse> imageUrls = allMembers.stream()
+                .map(ImageUrlResponse::of)
+                .toList();
+
+        return ApiResponse.success(imageUrls);
     }
 
     @PreAuthorize("hasRole('ADMIN')")
