ffcdb0e5c540c9c20f803c362db9961c0fff9186
taemin6536 <xoals6536@naver.com>
2025-06-25 22:49:14 +0900

    feat: 암호화 유틸클래스 개발 추후 반영 여부 고민

---
+++ core/core-api/src/main/java/io/dodn/springboot/common/util/EncryptUtil.java
@@ -0,0 +1,47 @@
+package io.dodn.springboot.common.util;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+public class EncryptUtil {
+    private static final String ALGORITHM = "AES";
+
+    @Value("${encrypt.key}")
+    private String keyString; // 32바이트 키
+
+    private static byte[] KEY;
+
+    @PostConstruct
+    public void init() {
+        KEY = Base64.getDecoder().decode(keyString);
+    }
+
+    public static String encrypt(String value) {
+        try {
+            SecretKeySpec secretKey = new SecretKeySpec(KEY, ALGORITHM);
+            Cipher cipher = Cipher.getInstance(ALGORITHM);
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            byte[] encryptedByteValue = cipher.doFinal(value.getBytes(StandardCharsets.UTF_8));
+            return Base64.getEncoder().encodeToString(encryptedByteValue);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String decrypt(String value) {
+        try {
+            SecretKeySpec secretKey = new SecretKeySpec(KEY, ALGORITHM);
+            Cipher cipher = Cipher.getInstance(ALGORITHM);
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            byte[] decryptedByteValue = Base64.getDecoder().decode(value);
+            return new String(cipher.doFinal(decryptedByteValue), StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
